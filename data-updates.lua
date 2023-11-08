a54fc9288738e229e05815996a403d745fea9147
Soul-Burn <sugoibaka@gmail.com>
2023-11-08 22:40:07 +0200

    Small import organization

--- data-updates.lua
+++ data-updates.lua
@@ -9,17 +9,25 @@ for _, qm in pairs(libq.quality_modules) do
     lib.add_prototype({ type = "sprite", name = "jq_quality_module_icon_" .. qm.name, filename = qm.icon, size = 64, scale = 0.5 })
 end
 
+lib.flush_prototypes()
+
 log("Recipe count before JQ: " .. lib.table_size(data.raw.recipe))
 
-require(lib.p.prot .. "quality_modules")
-require(lib.p.prot .. "recycling")
-require(lib.p.prot .. "resources")
-require(lib.p.prot .. "quality_enhancing_machines")
-require(lib.p.prot .. "recipe_qualities")
-require(lib.p.prot .. "tiles")
-require(lib.p.prot .. "items_and_entities")
-require(lib.p.prot .. "recipes_with_quality_upgrades")
-require(lib.p.prot .. "quality_enhancing_machine_programming")
-require(lib.p.prot .. "technologies")
+local imports = {
+    "quality_modules",
+    "recycling",
+    "resources",
+    "quality_enhancing_machines",
+    "recipe_qualities",
+    "tiles",
+    "items_and_entities",
+    "recipes_with_quality_upgrades",
+    "quality_enhancing_machine_programming",
+    "technologies",
+}
+
+for _, import in pairs(imports) do
+    require(lib.p.prot .. import)
+end
 
 log("Recipe count after JQ: " .. lib.table_size(data.raw.recipe))
