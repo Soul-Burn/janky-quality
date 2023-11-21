1059303ba569e1abb1536ca5546d7b789be13d1b
Soul-Burn <sugoibaka@gmail.com>
2023-11-21 21:57:22 +0200

    Fixed extra qualities breaking

--- prototypes/quality_bonuses_extra.lua
+++ prototypes/quality_bonuses_extra.lua
@@ -3,7 +3,7 @@ local m = require(lib.p.prot .. "entity_mods")
 
 return {
     ["__all__"] = m.mod { ["fuel_value.?"] = m.energy(0.3) },
-    ["lamp"] = m.mod { ["light.size"] = m.mult(0.3), ["light_when_colored.size"] = m.mult(0.3) },
-    ["mining-drill"] = m.default_mod("mining_speed"),
+    ["lamp"] = m.default_mod { "light.size", "light_when_colored.size" },
+    ["mining-drill"] = m.default_mod { "mining_speed" },
     ["pipe-to-ground"] = m.mod { ["fluid_box.pipe_connections.2.max_underground_distance"] = m.add(2) },
 }
