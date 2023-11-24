e37f54e8adc31d1c82cb1d6bfeccf39cb94d14a7
Soul-Burn <sugoibaka@gmail.com>
2023-11-24 12:42:28 +0200

    Small extra walking speed bonuses for quality tiles + tints

--- prototypes/quality_bonuses_extra.lua
+++ prototypes/quality_bonuses_extra.lua
@@ -6,4 +6,16 @@ return {
     ["lamp"] = m.default_mod { "light.size", "light_when_colored.size" },
     ["mining-drill"] = m.default_mod { "mining_speed" },
     ["pipe-to-ground"] = m.mod { ["fluid_box.pipe_connections.2.max_underground_distance"] = m.add(2) },
+    ["tile"] = function(p, quality)
+        if p.walking_speed_modifier then
+            p.walking_speed_modifier = p.walking_speed_modifier * (1.0 + 0.1 * quality.modifier)
+        end
+        if p.vehicle_friction_modifier then
+            p.vehicle_friction_modifier = p.vehicle_friction_modifier * (1.0 + 0.1 * quality.modifier)
+        end
+        p.tint = {}
+        for i, c in pairs(quality.color) do
+            p.tint[i] = 0.25 * (c - 1) + 1
+        end
+    end
 }
