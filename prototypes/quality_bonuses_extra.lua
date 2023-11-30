c63811f99f7860e9d6706b220c4315660983a676
Soul-Burn <sugoibaka@gmail.com>
2023-11-30 11:31:43 +0200

    Fixed certain lamps failing bonuses

--- prototypes/quality_bonuses_extra.lua
+++ prototypes/quality_bonuses_extra.lua
@@ -3,7 +3,7 @@ local m = require(lib.p.prot .. "entity_mods")
 
 return {
     ["__all__"] = m.mod { ["fuel_value.?"] = m.energy(0.3) },
-    ["lamp"] = m.default_mod { "light.size", "light_when_colored.size" },
+    ["lamp"] = m.default_mod { "light.size.?", "light_when_colored.size.?" },
     ["mining-drill"] = m.default_mod { "mining_speed" },
     ["pipe-to-ground"] = m.mod { ["fluid_box.pipe_connections.2.max_underground_distance"] = m.add(2) },
     ["tile"] = function(p, quality)
