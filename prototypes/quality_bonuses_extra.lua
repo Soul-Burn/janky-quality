d0709bc5cdcbc297eed50717d27ff57211d9e5fc
Soul-Burn <sugoibaka@gmail.com>
2024-01-16 21:15:31 +0200

    Extra quality bonus for underground pipe now supports weird underground pipes

--- prototypes/quality_bonuses_extra.lua
+++ prototypes/quality_bonuses_extra.lua
@@ -5,7 +5,7 @@ return {
     ["__item__"] = m.mod { ["fuel_value.?"] = m.energy(0.3) },
     ["lamp"] = m.default_mod { "light.size.?", "light_when_colored.size.?" },
     ["mining-drill"] = m.default_mod { "mining_speed" },
-    ["pipe-to-ground"] = m.mod { ["fluid_box.pipe_connections.2.max_underground_distance"] = m.add(2) },
+    ["pipe-to-ground"] = m.mod { ["fluid_box.pipe_connections.2.max_underground_distance.?"] = m.add(2) },
     ["tile"] = function(p, quality)
         if p.walking_speed_modifier then
             p.walking_speed_modifier = p.walking_speed_modifier * (1.0 + 0.1 * quality.modifier)
