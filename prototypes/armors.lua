539891cdc62e91735cba8731211f8425ac1965b6
Soul-Burn <sugoibaka@gmail.com>
2024-01-29 22:05:51 +0200

    Support "no quality" on armors

--- prototypes/armors.lua
+++ prototypes/armors.lua
@@ -5,8 +5,10 @@ for _, character in pairs(data.raw.character) do
         if animation.armors then
             local new_armors = {}
             for _, armor in pairs(animation.armors) do
-                for _, q in pairs(libq.qualities) do
-                    table.insert(new_armors, libq.name_with_quality(armor, q))
+                if not libq.forbids_quality(armor) then
+                    for _, q in pairs(libq.qualities) do
+                        table.insert(new_armors, libq.name_with_quality(armor, q))
+                    end
                 end
             end
             animation.armors = new_armors
